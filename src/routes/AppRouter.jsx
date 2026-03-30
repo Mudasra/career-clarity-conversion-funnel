8638652723a0404bb873172fe0fbac2397796251
Mudasra <km4446245@gmail.com>
2026-03-31 00:07:49 +0500

    App router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Navbar from "../components/Navbar/Navbar";
 import ThankYouPage from "../pages/ThankYouPage";
 import LandingPage from "../pages/LandingPage";
+import BookCallPage from "../pages/BookCallPage";
 
 export default function AppRouter () {
     return (
@@ -10,6 +11,7 @@ export default function AppRouter () {
         <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/thank-you" element={<ThankYouPage />} />
+            <Route path="/book-call" element={<BookCallPage />} />
             <Route path="*" element={<LandingPage />}/>
         </Routes>
         </BrowserRouter>
